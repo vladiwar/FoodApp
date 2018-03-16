07841770439a8dedcba178e3ea923043f62e9ccc
vladiwar <vladiwar1@gmail.com>
2018-03-16 02:22:03 -0600

    Montiel Garcia Joseph Vladimir Copiado

--- app/src/main/java/com/upp/foodapp/MainActivity.java
+++ app/src/main/java/com/upp/foodapp/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    //Montiel Garcia Vladimir
+    //Montiel Garcia Joseph  Vladimir
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
